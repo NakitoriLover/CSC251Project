7da1a06fe4c2a26e9ec14add7098009a0ec04d64
NakitoriLover <irose001@email.cpcc.edu>
2026-08-28 16:39:11 -0400

    Policy Class Completed
    Added code for demo class

--- Policy.java
+++ Policy.java
@@ -1,3 +1,168 @@
 public class Policy
 {
+   //instance fields
+   private int policyNum, holderAge;
+   private String providerName, holderFirstName, holderLastName, smokingStatus;
+   private double holderHeight, holderWeight;
+   
+   //no arg constructor
+   public Policy()
+   {
+      policyNum = 0; 
+      holderAge = 0;
+      providerName = "";
+      holderFirstName = "";
+      holderLastName = "";
+      smokingStatus = "";
+      holderHeight = 0.0;
+      holderWeight = 0.0;
+   }
+   
+   //basic arg constructors
+   public Policy(int a)
+   {
+      holderAge = a;
+   }
+   
+   public Policy(String a)
+   {
+      smokingStatus = a;
+   }
+   
+   public Policy(double a, double b)
+   {
+      holderHeight = a;
+      holderWeight = b;
+   }
+   
+   //all arg constructor
+   public Policy(int a, int b, String c, String d, String e, String f, double g, double h)
+   {
+      policyNum = a; 
+      holderAge = b;
+      providerName = c;
+      holderFirstName = d;
+      holderLastName = e;
+      smokingStatus = f;
+      holderHeight = g;
+      holderWeight = h;
+   }
+   
+   //setter and return methods for policy number
+   public void setPolicyNum(int a)
+   {
+      policyNum = a;
+   }
+   
+   public int returnPolicyNum()
+   {
+      return policyNum;
+   }
+   
+   //setter and return methods for policy holders age
+   public void setHolderAge(int a)
+   {
+      holderAge = a;
+   }
+   
+   public int returnHolderAge()
+   {
+      return holderAge;
+   }
+   
+   //setter and return methods for insurance providers name
+    public void setProviderName(String a)
+   {
+      providerName = a;
+   }
+   
+   public String returnProviderName()
+   {
+      return providerName;
+   }
+   
+    //setter and return methods for policy holders first name
+   public void setHolderFirstName(String a)
+   {
+      holderFirstName = a;
+   }
+   
+   public String returnHolderFirstName()
+   {
+      return holderFirstName;
+   }
+   
+   //setter and return methods for policy holders last name
+   public void setHolderLastName(String a)
+   {
+      holderLastName = a;
+   }
+   
+   public String returnHolderLastName()
+   {
+      return holderLastName;
+   }
+   
+   //setter and return methods for policy holders smoking status
+   public void setSmokingStatus(String a)
+   {
+      smokingStatus = a;
+   }
+   
+   public String returnSmokingStatus()
+   {
+      return smokingStatus;
+   }
+   
+   //setter and return methods for policy holders height
+   public void setHolderHeight(double a)
+   {
+      holderHeight = a;
+   }
+   
+   public double returnHolderHeight()
+   {
+      return holderHeight;
+   }
+   
+   //setter and return methods for policy holders weight
+   public void setHolderWeight(double a)
+   {
+      holderWeight = a;
+   }
+   
+   public double returnHolderWeight()
+   {
+      return holderWeight;
+   }
+   
+   public double calculateBMI()
+   {
+      double BMI = (holderWeight * 703) / (holderHeight * holderHeight);
+      return BMI;
+   }
+   
+   public double calculateCost()
+   {
+      double totalCost = 600;
+      double BMI = calculateBMI();
+      
+      if(holderAge > 50)
+      {
+         totalCost += 75;
+      }
+      
+      if(smokingStatus.equalsIgnoreCase("smoker"))
+      {
+         totalCost += 100;
+      }
+      
+      if(BMI > 35)
+      {
+         totalCost += (BMI - 35) * 20;
+      }
+      
+      return totalCost;
+   }
+   
 }
--- Project_Morgan_Rose.java
+++ Project_Morgan_Rose.java
@@ -1,3 +1,95 @@
+import java.util.*;
+
 public class Project_Morgan_Rose
 {
+   public static void main(String[] args)
+   {
+      Scanner keyboard = new Scanner(System.in);
+      
+      //variables
+      int policyNum, holderAge = 0;
+      String providerName, holderFirstName, holderLastName, smokingStatus = "";
+      double holderHeight, holderWeight, BMI, policyPrice = 0.0;
+      
+      //a new policy instance is created at the beginning of the program
+      Policy policyholder = new Policy();
+      
+      //user is prompted for basic info
+      System.out.print("Please enter the Policy Number: ");
+      policyNum = keyboard.nextInt();
+      
+      //program enters variable into policy instance
+      policyholder.setPolicyNum(policyNum);
+      System.out.println();
+      
+      //filler line to allow line input to function
+      keyboard.nextLine();
+      
+      //this cycle will be generally repeated until line 76 unless noted
+      System.out.print("Please enter the Provider Name: ");
+      providerName = keyboard.nextLine();
+      policyholder.setProviderName(providerName);
+      System.out.println();
+      
+      System.out.print("Please enter the Policyholder's First Name: ");
+      holderFirstName = keyboard.nextLine();
+      policyholder.setHolderFirstName(holderFirstName);
+      System.out.println();
+      
+      System.out.print("Please enter the Policyholder's Last Name: ");
+      holderLastName = keyboard.nextLine();
+      policyholder.setHolderLastName(holderLastName);
+      System.out.println();
+      
+      System.out.print("Please enter the Policyholder's Age: ");
+      holderAge = keyboard.nextInt();
+      policyholder.setHolderAge(holderAge);
+      System.out.println();
+      
+      //filler line to allow line input to function
+      keyboard.nextLine();
+      
+      System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
+      smokingStatus = keyboard.nextLine();
+      
+      //program will validate user input
+      while(! "smoker".equalsIgnoreCase(smokingStatus) && ! "non-smoker".equalsIgnoreCase(smokingStatus))
+      {
+         System.out.println();
+         System.out.println("Invalid input, you MUST enter either smoker or non-smoker: ");
+         System.out.println();
+         System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
+         smokingStatus = keyboard.nextLine();
+      }
+      
+      policyholder.setSmokingStatus(smokingStatus);
+      System.out.println();
+      
+      System.out.print("Please enter the Policyholder's Height (in inches): ");
+      holderHeight = keyboard.nextDouble();
+      policyholder.setHolderHeight(holderHeight);
+      System.out.println();
+      
+      System.out.print("Please enter the Policyholder's Weight (in pounds): ");
+      holderWeight = keyboard.nextDouble();
+      policyholder.setHolderWeight(holderWeight);
+      System.out.println();
+            
+      //bmi and policy price is calculated near the end
+      BMI = policyholder.calculateBMI();
+      policyPrice = policyholder.calculateCost();
+      
+      //all information is displayed to user
+      System.out.println("Policy Number: " + policyholder.returnPolicyNum());
+      System.out.println("Provider Name: " + policyholder.returnProviderName());
+      System.out.println("Policyholder's First Name: " + policyholder.returnHolderFirstName());
+      System.out.println("Policyholder's Last Name: " + policyholder.returnHolderLastName());
+      System.out.println("Policyholder's Age: " + policyholder.returnHolderAge());
+      System.out.println("Policyholder's Smoking Status: " + policyholder.returnSmokingStatus());
+      System.out.println("Policyholder's Height: " + policyholder.returnHolderHeight() + " inches");
+      System.out.println("Policyholder's Weight: " + policyholder.returnHolderWeight() + " pounds");
+      System.out.printf("Policyholder's BMI: %.2f\n", BMI);
+      System.out.printf("Policyholder's Price: $%.2f\n", policyPrice);
+      
+   }
 }
